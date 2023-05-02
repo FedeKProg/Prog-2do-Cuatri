13cb8c68bc8168d25482ec2815dd19452746bb22
FedeKProg <fedekatriniuk@gmail.com>
2023-05-02 00:02:48 -0300

    update

--- Clase02/Clase02Repaso/EjercicioRepaso/Program.cs
+++ Clase02/Clase02Repaso/EjercicioRepaso/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace EjercicioRepaso
+﻿using System.Linq;
+using Entidades;
+namespace EjercicioRepaso
 {
     internal class Program
     {
@@ -13,42 +15,24 @@
             Console.WriteLine("Ingrese el nombre de la mascota");
             NombreMascota = Console.ReadLine();
 
-            Console.WriteLine("Ingrese la edad de la mascota (1-12)");
-            int.TryParse(Console.ReadLine(), out edadMascota);
-            while (edadMascota < 1 || edadMascota > 12)
-            {
-                Console.WriteLine("La edad ingresada no es valida. Ingrese un valor entre 1-12");
-                int.TryParse(Console.ReadLine(), out edadMascota);
-            }
 
-            Console.WriteLine("Ingrese el tipo de la mascota de la mascota (gato, perro o hamster)");
-            tipoMascota = Console.ReadLine();
-            while (tipoMascota != "gato" && tipoMascota != "perro" && tipoMascota != "hamster")
-            {
-                Console.WriteLine("el tipo ingresado no es valido. Ingrese un valor entre (gato, perro o hamster)");
-                tipoMascota = Console.ReadLine();
-            }
+            Validador.validarNumero("Ingrese la edad de la mascota (1-12)", out edadMascota, 1, 12);
 
-            Console.WriteLine("Ingrese el peso de la mascota");
-            int.TryParse(Console.ReadLine(), out pesoMascota);
-            while (pesoMascota < 0 || pesoMascota > 50)
-            {
-                Console.WriteLine("El peso ingresado no es valido. Ingrese un valor entre 0-50");
-                int.TryParse(Console.ReadLine(), out pesoMascota);
-            }
 
-            Console.WriteLine("Ingrese el diagnostico de la mascota de la mascota (otitis, alergias, parasitos)");
-            diagnostico = Console.ReadLine();
-            while (diagnostico != "otitis" && diagnostico != "alergias" && diagnostico != "parasitos")
-            {
-                Console.WriteLine("el tipo ingresado no es valido. Ingrese el diagnostico de la mascota de la mascota (otitis, alergias, parasitos)");
-                diagnostico = Console.ReadLine();
-            }
+            Validador.validarStrings("Ingrese el tipo de la mascota de la mascota (gato, perro o hamster)", out tipoMascota, "gato", "perro", "hamster");
+
+            Validador.validarNumero("Ingrese el peso de la mascota (0-50)", out pesoMascota, 0, 50);
+
+            Validador.validarStrings("Ingrese el diagnostico tipo de la mascota de la mascota (parasitos, otitis, alergias)", out diagnostico, "parasitos", "otitis", "alergias");
+
 
             Console.WriteLine(NombreMascota);
             Console.WriteLine(edadMascota);
             Console.WriteLine(tipoMascota);
+            Console.WriteLine(pesoMascota);
             Console.WriteLine(diagnostico);
+
+            Console.ReadKey();
         }
     }
 }
---
+++ Clase02/Clase02Repaso/Entidades/Validador.cs
@@ -0,0 +1,30 @@
+﻿namespace Entidades
+{
+    public class Validador
+    {
+        public static void validarNumero(string mensaje, out int valor, int min, int max)
+        {
+            Console.WriteLine(mensaje);
+
+            int.TryParse(Console.ReadLine(), out valor);
+            while (valor < min || valor > max)
+            {
+                Console.WriteLine($"El valor es invalido. Ingresar valor entre {min}-{max}");
+                Console.WriteLine(mensaje);
+                int.TryParse(Console.ReadLine(), out valor);
+            }
+        }
+        public static void validarStrings(string mensaje, out string valor, params string[] valores)
+        {
+            Console.WriteLine(mensaje);
+            valor = Console.ReadLine();
+
+            while (!valores.Contains(valor))
+            {
+                Console.WriteLine($"el tipo ingresado no es valido. Ingrese unvalor valido");
+                valor = Console.ReadLine();
+            }
+
+        }
+    }
+}
